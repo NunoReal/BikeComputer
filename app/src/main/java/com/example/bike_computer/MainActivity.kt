70bb205f1af8446330c5a412d51a00252632980d
NunoReal <numairkantarevic@outlook.de>
2025-08-14 09:55:57 +0200

    add permissions for location

--- app/src/main/java/com/example/bike_computer/MainActivity.kt
+++ app/src/main/java/com/example/bike_computer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.bike_computer
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.bike_computer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+            return
+        }
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -30,12 +52,14 @@ class MainActivity : AppCompatActivity() {
                         .commit()
                     true
                 }
+
                 R.id.nav_compass -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, CompassFragment())
                         .commit()
                     true
                 }
+
                 R.id.nav_training -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, TrainingFragment())
